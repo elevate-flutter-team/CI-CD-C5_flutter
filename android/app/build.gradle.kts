fb165132a94975157d5b243565fa0a5aaf278686
John Safwat <106314101+john-safwat@users.noreply.github.com>
2026-05-21 20:40:25 +0300

    - Add a GitHub Action to trigger Codemagic builds on release branches.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.elevate.ci_cd_c5_elevate"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.14033849"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
